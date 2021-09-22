2dc5dae4c6a0bc488b5a184d18b29afd928caecc
Alberto Basalo <albertobasalo@hotmail.com>
2021-09-22 17:48:05 +0200

    test: starting up message

--- src/main.rs
+++ src/main.rs
@@ -7,3 +7,15 @@ fn main() {
 fn get_start_line(program: &String) -> String {
     return format!("{}{}{}", "🚀 ", program, " started");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_should_get_start_line() {
+        let input = "test".to_string();
+        let actual = get_start_line(&input);
+        let expected = "🚀 test started";
+        assert_eq!(actual, expected);
+    }
+}
